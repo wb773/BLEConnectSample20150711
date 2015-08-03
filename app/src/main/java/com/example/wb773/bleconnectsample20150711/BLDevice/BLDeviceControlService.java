cc586f121197dba485dd5ae0f046ea8b64494b28
wb773 <wb773@wb773-MBA.local>
2015-08-03 20:04:46 +0900

    refs #1 テスト２

--- app/src/main/java/com/example/wb773/bleconnectsample20150711/BLDevice/BLDeviceControlService.java
+++ app/src/main/java/com/example/wb773/bleconnectsample20150711/BLDevice/BLDeviceControlService.java
@@ -1,20 +1,70 @@
 package com.example.wb773.bleconnectsample20150711.BLDevice;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import java.util.UUID;
 
 /**
  * Created by wb773 on 15/07/22.
  */
 public class BLDeviceControlService extends Service {
+    private final static String TAG = BLDeviceControlService.class.getSimpleName();
 
-    @Nullable
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private String mBluetoothDeviceAddress;
+    private BluetoothGatt mBluetoothGatt;
+    private int mConnectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
+    public final static UUID UUID_HEART_RATE_MEASUREMENT =
+            UUID.fromString(GattAttributes.HEART_RATE_MEASUREMENT);
+
+    //Binder
+    private final IBinder mBinder = new LocalBinder();
+
+
+    /** ----------------------------------------------------------------------
+     *  Binder
+     *  ---------------------------------------------------------------------- */
+
+    public class LocalBinder extends Binder {
+        BLDeviceControlService getService() {
+            return BLDeviceControlService.this;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
 
 
 }
